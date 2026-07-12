a18bc7aea3e5db45362a61c801b80650a57482a7
Zhanheng Guo <3526145827@qq.com>
2026-07-13 02:38:11 +0800

    chore: 移除误提交的临时排查脚本

--- _clean_dryrun.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set +e
-cd /root/ai_hubs
-echo "===== dry-run: git clean -fdx 但排除 data/venv/.env/*.log ====="
-git clean -ndx -e data -e venv -e '.env' -e '*.log' | head -60
-echo "DRYRUN_DONE"
